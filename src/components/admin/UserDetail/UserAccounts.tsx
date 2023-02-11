11d89f33b65b6d023d7bc569d9c2b5928c331f1b
Minho Kim <music4537@naver.com>
2023-02-11 18:37:42 +0900

    no change

--- src/components/admin/UserDetail/UserAccounts.tsx
+++ src/components/admin/UserDetail/UserAccounts.tsx
@@ -1,13 +1,17 @@
 import Sidebar from "./Sidebar";
+import searchIcon from "../../../asset/functionIcon/search.png";
+import PinBankLogo from "../../../asset/pinbank_icon.png";
 
 const UserAccounts = () => {
   return (
     <>
       <div className="flex">
         <Sidebar></Sidebar>
-        <h1 className="py-5 text-2xl text-pintalk-dark-brown text-semibold">
-          님의 계좌 정보입니다
-        </h1>
+        <div className="flex ml-6 mt-6">
+          <img className="object-contain h-7 pr-1 w-7 flex" src={searchIcon}></img>
+          <h1 className="text-kukmin-dark-brown font-bold text-xl">계좌조회</h1>
+        </div>
+        {/*<hr className="h-0.5 my-8 bg-pintalk-dark-brown border-0 ml-6 w-[80%]" />*/}
       </div>
     </>
   );
