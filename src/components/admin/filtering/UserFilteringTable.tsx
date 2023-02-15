f2255fabfd7bca59f876a58c7f0ee7c01db7c0ea
taehee <jangth0655@gmail.com>
2023-02-15 21:11:07 +0900

    style : admin-필터링 검색의 날짜 값 포멧 변경

--- src/components/admin/filtering/UserFilteringTable.tsx
+++ src/components/admin/filtering/UserFilteringTable.tsx
@@ -66,7 +66,7 @@ const UserFilteringTable = ({ emailCodeGroup }: Props) => {
     const year = new Date(date).getFullYear() + "";
     const month = new Date(date).toLocaleDateString("en", { month: "2-digit" });
     const day = new Date(date).toLocaleDateString("en", { day: "2-digit" });
-    return year + "-" + month + "-" + day;
+    return year + month + day;
   };
 
   const onValid = (data: UserFilteringData) => {
