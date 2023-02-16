3754911220864494008caabcd4f2868b38cd720c
Minho Kim <music4537@naver.com>
2023-02-16 12:55:16 +0900

    feat : added account list view page on admin

--- src/components/admin/table/AccountListTable.tsx
+++ src/components/admin/table/AccountListTable.tsx
@@ -5,8 +5,6 @@ import UserAccounts from "../userAccounts";
 const AccountListTable = () => {
   const { accounts: accountList, error, PageInfo, loading } = useAccount();
 
-  console.log(accountList);
-
   return (
     <div>
       <AccountFilteringTable />
@@ -14,25 +12,58 @@ const AccountListTable = () => {
         <table className="w-full">
           <thead className="bg-white ">
             <tr>
-              <th className="w-10 p-3 text-sm font-semi-bold tracking-wide text-left">고유번호</th>
-              <th className="w-10 p-3 text-sm font-semi-bold tracking-wide text-left">성별</th>
-              <th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">이름</th>
+              <th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">예금주명</th>
+              <th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">계좌구분</th>
+              <th className="w-10 p-3 text-sm font-semi-bold tracking-wide text-left">계좌별명</th>
+              <th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">계좌잔액</th>
+              {/*<th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">Account Hists</th>*/}
+              <th className="w-24 p-3 text-sm font-semi-bold tracking-wide text-left">계좌번호</th>
+              <th className="w-20 p-3 text-sm font-semi-bold tracking-wide text-left">
+                계좌종류
+              </th>{" "}
+              {/*계좌종류(‘1’:수시입출금, ‘2’:예적금, ‘6’:수익증권, ‘T’:종합계좌)*/}
+              <th className="w-10 p-3 text-sm font-semi-bold tracking-wide text-left">대표코드</th>
+              <th className="w-10 p-3 text-sm font-semi-bold tracking-wide text-left">점별코드</th>
+              <th className="w-24 p-3 text-sm font-semi-bold tracking-wide text-left">
+                출금(개설)기관명
+              </th>
+              {/*<th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">*/}
+              {/*  계좌개설 일시*/}
+              {/*</th>*/}
               <th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">
-                주민등록번호
+                핀테크이용번호
               </th>
-              <th className="w-20 p-3 text-sm font-semi-bold tracking-wide text-left">전화번호</th>
-              <th className="w-48 p-3 text-sm font-semi-bold tracking-wide text-left">주소</th>
-              <th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">이메일</th>
-              <th className="w-24 p-3 text-sm font-semi-bold tracking-wide text-left">가입일자</th>
-              <th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">
-                정보변경일자
+              {/*<th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">*/}
+              {/*  조회서비스 동의일시*/}
+              {/*</th>*/}
+              <th className="w-24 p-3 text-sm font-semi-bold tracking-wide text-left">
+                조회서비스 동의여부
               </th>
+              {/*<th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">*/}
+              {/*  계좌수정 일시*/}
+              {/*</th>*/}
+              {/*<th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">*/}
+              {/*  계좌_SEQ*/}
+              {/*</th>*/}
+              {/*<th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">*/}
+              {/*  가입날짜*/}
+              {/*</th>*/}
+              <th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">
+                개별저축은행명
+              </th>
+              <th className="w-16 p-3 text-sm font-semi-bold tracking-wide text-left">계좌상태</th>
+              <th className="w-24 p-3 text-sm font-semi-bold tracking-wide text-left">
+                출금서비스 동의여부
+              </th>
+              {/*<th className="w-32 p-3 text-sm font-semi-bold tracking-wide text-left">*/}
+              {/*  정보수정 날짜*/}
+              {/*</th>*/}
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100">
-            {/*{accountList?.map((account) => {*/}
-            {/*  return <UserAccounts account={account} key={account.no} />;*/}
-            {/*})}*/}
+            {accountList?.map((account) => {
+              return <UserAccounts account={account} key={account.no} />;
+            })}
           </tbody>
         </table>
       </div>
--- src/components/admin/userAccounts.tsx
+++ src/components/admin/userAccounts.tsx
@@ -13,24 +13,45 @@ const UserAccounts = ({ account }: Props) => {
   //   );
   // };
 
+  const account_Type = account?.accountType;
+
   return (
     <tr
       className="bg-gray-50 hover:text-white hover:bg-pintalk-dark-brown cursor-pointer"
       // onClick={handleNewWindow}
     >
       <td className="p-3 text-sm whitespace-nowrap">{account?.holderName}</td>
-      <td className="p-3 text-sm whitespace-nowrap">{account?.holderType}</td>
+      <td className="p-3 text-sm whitespace-nowrap">
+        {account?.holderType === "P" ? "개인" : "법인"}
+      </td>
       <td className="p-3 text-sm whitespace-nowrap">{account?.accountAlias}</td>
       <td className="p-3 text-sm whitespace-nowrap">{account?.accountBalance}</td>
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.accountHists}</td>*/}
       <td className="p-3 text-sm whitespace-nowrap">{account?.accountNo}</td>
-      <td className="p-3 text-sm whitespace-nowrap">{account?.accountType}</td>
-      <td className="p-3 text-sm whitespace-nowrap">{account?.bankName}</td>
+      <td className="p-3 text-sm whitespace-nowrap">
+        {
+          {
+            "1": "수시입출금",
+            "2": "예적금",
+            "6": "수익증권",
+            T: "종합계좌",
+          }[account_Type]
+        }
+      </td>
       <td className="p-3 text-sm whitespace-nowrap">{account?.bankCodeStd}</td>
       <td className="p-3 text-sm whitespace-nowrap">{account?.bankCodeSub}</td>
-      <td className="p-3 text-sm whitespace-nowrap">{account?.accountBalance}</td>
+      <td className="p-3 text-sm whitespace-nowrap">{account?.bankName}</td>
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.createDt}</td>*/}
+      <td className="p-3 text-sm whitespace-nowrap">{account?.finTechUseNum}</td>
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.inquiryAgreeDt}</td>*/}
       <td className="p-3 text-sm whitespace-nowrap">{account?.inquiryAgreeYn}</td>
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.modifyDt}</td>*/}
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.no}</td>*/}
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.reg_Date}</td>*/}
       <td className="p-3 text-sm whitespace-nowrap">{account?.savingsBankName}</td>
       <td className="p-3 text-sm whitespace-nowrap">{account?.state}</td>
+      <td className="p-3 text-sm whitespace-nowrap">{account?.transferAgree_yn}</td>
+      {/*<td className="p-3 text-sm whitespace-nowrap">{account?.update_Date}</td>*/}
     </tr>
   );
 };
